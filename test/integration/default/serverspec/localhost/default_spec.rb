e0448889c86bb0f07fbf1bd3bcc26c8a24846b32
AsIf <iamthe.angrytoast@gmail.com>
2016-02-15 16:26:19 -0800

    directory exists?

---
+++ test/integration/default/serverspec/localhost/default_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+describe 'ansible-varnish::default' do
+
+  describe port(80) do
+    it { should be_listening }
+  end
+
+  describe port(120) do
+    it { should be_listening }
+  end
+
+  describe port(6082) do
+    it { should be_listening.with('localhost') }
+  end
+
+end
+
+describe 'ansible-varnish::install' do
+
+  describe package('varnish') do
+    it { should be_installed.by('apt') }
+  end
+
+end
+
+describe 'ansible-varnish::configure' do
+
+  describe file('/etc/default/varnish') do
+    it { should be_directory }
+    it { should be_mode 664 }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'staff' }
+    its(:content) { should match '/templates/etc/default/varnish'}
+  end
+
+  describe file('/etc/varnish/default.vcl') do
+    it { should be_directory }
+    it { should be_mode 664 }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'staff' }
+    its(:content) { should match '/templates/etc/varnish/default.vcl'}
+  end
+
+  describe service('varnish') do
+    it { should be_installed }
+    it { should be_running }
+  end
+
+end
